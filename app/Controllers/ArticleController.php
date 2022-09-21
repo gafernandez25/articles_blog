1cf9ddcb468e24e690567620e598a6d31f714609
Guillermo Fernandez <gafernandez2525@gmail.com>
2022-09-21 13:53:40 +0200

    Create form validator for store data method in ArticleController

--- app/Controllers/ArticleController.php
+++ app/Controllers/ArticleController.php
@@ -2,12 +2,32 @@
 
 namespace App\Controllers;
 
+use App\DBPdo;
+use App\FormValidators\ArticleControllerValidator;
+use App\Interfaces\ArticleRepositoryInterface;
+use App\Interfaces\DBPdoInterface;
+use App\Services\ArticleService;
+
 class ArticleController
 {
+    public function __construct(
+//        private DBPdo $db,     //change to interface and bind param somewhere else
+//        private ArticleRepositoryInterface $articleRepository,
+        private ArticleService $articleService,
+        private ArticleControllerValidator $validator
+    ) {
+    }
 
     public function index()
     {
-        echo "Articles' index";
+        //check for paginator params
+
+        //get articles
+        $articles = $this->articleService->getArticles(3, 0);
+
+        //return to view
+        echo "<pre>";
+        print_r($articles);
     }
 
     public function show()
@@ -21,6 +41,14 @@ class ArticleController
 
     public function store()
     {
+        $validatedData = $this->validator->storeData($_POST);
+
+        $data = [
+            "userId" => $validatedData["user"],
+            "title" => $validatedData["title"]
+        ];
+
+        $article = $this->articleService->createArticle($data);
     }
 
     public function edit()
--- app/FormValidators/ArticleControllerValidator.php
+++ app/FormValidators/ArticleControllerValidator.php
@@ -5,4 +5,15 @@ namespace App\FormValidators;
 class ArticleControllerValidator
 {
 
+    public function storeData($data)
+    {
+        if (empty($data["user"]) && is_numeric($data["user"])) {
+            return false;
+        }
+        if (empty($data["title"])) {
+            return false;
+        }
+        return $data;
+    }
+
 }
