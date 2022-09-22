0510285dd29811e5998f9e121315ad4755aadf81
Guillermo Fernandez <gafernandez2525@gmail.com>
2022-09-22 21:12:37 +0200

    Use wildcards where possible in container config file

--- config/container.php
+++ config/container.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'App\Interfaces\ArticleRepositoryInterface' => DI\autowire('App\Repositories\ArticlePdoRepository'),
+    'App\Interfaces\*RepositoryInterface' => DI\autowire('App\Repositories\*PdoRepository'),
     'App\Interfaces\DBPdoInterface' => DI\autowire("App\DBPdo"),
-    "App\Interfaces\UserRepositoryInterface"=>DI\autowire("App\Repositories\UserPdoRepository")
 ];
